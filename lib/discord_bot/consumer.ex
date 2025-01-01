36afb95d1c868065915bea88e2211f42cd00a951
Josh Чернов <961877+joshchernoff@users.noreply.github.com>
2025-01-01 01:07:25 -0800

    consolidate

--- lib/discord_bot/consumer.ex
+++ lib/discord_bot/consumer.ex
@@ -4,36 +4,27 @@ defmodule DiscordBot.Consumer do
 
   def handle_event({:MESSAGE_CREATE, msg, _ws_state}) do
     case msg.content do
-      "!elixir fn " <> function_string ->
-        send_info(:function, msg.channel_id, function_string)
-
-      "!elixir t " <> function_string ->
-        send_info(:type, msg.channel_id, function_string)
-
-      "!elixir g " <> function_string ->
-        send_info(:macro, msg.channel_id, function_string)
-
-      "!elixir c " <> function_string ->
-        send_info(:callback, msg.channel_id, function_string)
+      "!elixir " <> function_string ->
+        send_info(msg.channel_id, function_string)
 
       _ ->
         :ignore
     end
   end
 
-  defp send_info(kind, channel_id, function_string) do
+  defp send_info(channel_id, function_string) do
     # Parse the function string to get the module and function name
     [module_name, function_name] = String.split(function_string, ".", parts: 2)
     function = String.to_atom(function_name)
 
     # Get documentation for the function
-    result = ElixirDocs.get_docs(kind, module_name, function)
+    result = ElixirDocs.get_docs(module_name, function)
     Api.create_message(channel_id, result)
   end
 end
 
 defmodule ElixirDocs do
-  def get_docs(kind, module_name, function_name) do
+  def get_docs(module_name, function_name) do
     # Construct the module literal dynamically from the string
     module = :"Elixir.#{module_name}"
 
@@ -45,7 +36,7 @@ defmodule ElixirDocs do
       {:docs_v1, _, :elixir, _, %{"en" => module_doc}, _, docs} ->
         docs
         |> Enum.filter(fn
-          {{^kind, fun, _arrty}, _, _, _, _} ->
+          {{_, fun, _arrty}, _, _, _, _} ->
             fun == function_name
 
           {_, _, _, _, _} ->
@@ -61,16 +52,16 @@ defmodule ElixirDocs do
               functions |> dbg(),
               fn
                 {{:function, fun, arrty}, _, [sig], %{"en" => desc}, _} ->
-                  "[#{module_name}.#{sig}](https://hexdocs.pm/elixir/1.18.1/#{module_name}.html##{fun}/#{arrty})"
+                  "[Function: #{module_name}.#{sig}](https://hexdocs.pm/elixir/1.18.1/#{module_name}.html##{fun}/#{arrty})"
 
                 {{:type, fun, arrty}, _, _, _, _} ->
-                  "[#{module_name}.#{fun}](https://hexdocs.pm/elixir/1.18.1/#{module_name}.html#t#{fun}/#{arrty})"
+                  "[Type: #{module_name}.#{fun}](https://hexdocs.pm/elixir/1.18.1/#{module_name}.html#t#{fun}/#{arrty})"
 
                 {{:macro, fun, arrty}, _, _, _, _} ->
-                  "[#{module_name}.#{fun}/#{arrty}](https://hexdocs.pm/elixir/1.18.1/#{module_name}.html##{fun}/#{arrty})"
+                  "[Guard: #{module_name}.#{fun}/#{arrty}](https://hexdocs.pm/elixir/1.18.1/#{module_name}.html##{fun}/#{arrty})"
 
                 {{:callback, fun, arrty}, _, _, _, _} ->
-                  "[#{module_name}.#{fun}/#{arrty}](https://hexdocs.pm/elixir/1.18.1/#{module_name}.html#c#{fun}/#{arrty})"
+                  "[Callback: #{module_name}.#{fun}/#{arrty}](https://hexdocs.pm/elixir/1.18.1/#{module_name}.html#c#{fun}/#{arrty})"
               end
             )
             |> Enum.join("\n")
